f745ff2a2fcbc46e41c5b34ebaa59b21e647d50e
Renata Costa do Nascimento <renatacostarenatacostadonascimento@eaportal.org>
2023-11-30 00:16:50 -0300

    instalação do handlebars e do nodemon

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express")
+const exphbs = require("express-handlebars")
 
 const app = express ()
 
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
 app.get('/' , (requisicao, resposta) => {
-    resposta.send("Olá, mundo!")
+    resposta.send("Olá, mundo!");  
 })
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000!")
 })
-    
